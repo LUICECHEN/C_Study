6cb9143a431a371a3d30b1f854e27208ab89bff2
LUICECHEN <1342993482@qq.com>
2025-10-08 23:25:40 +0800

    获取某个范围的随机值

--- day01/randomNum.cpp
+++ day01/randomNum.cpp
@@ -12,6 +12,19 @@ int main(void){
     j = rand();
     cout << "j : " << j << endl;
   }
+
+  //生成某个范围(左右都是闭区间)类比植树问题，两边都取就要+1的随机数 rand() % (max - min + 1) + min
+  int min, max;
+  cout << "请输入最小值" << endl;
+  cin >> min;
+  cout << "请输入最大值" << endl;
+  cin >> max;
+  for (i = 0; i < 20; i++){
+    j = rand() % (max - min + 1) + min;
+    cout << min << "~" << max <<"的随机数：" << j << endl;
+  }
+
   return 0;
 
 }
+
