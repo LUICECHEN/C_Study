32061723d9e0784a88dbfe81b4c0482c9205c100
LUICECHEN <1342993482@qq.com>
2025-10-09 15:01:14 +0800

    标准错误流

---
+++ day02/cerr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+  char str[] = "Unable to read...";
+
+  cerr << "Error: " << str << endl;
+  
+  cout << "Hello World" << endl;
+
+  return 0;
+}
