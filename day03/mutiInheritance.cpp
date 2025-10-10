365861f3ecc9822c0b30226bdcc32055900b9209
LUICECHEN <1342993482@qq.com>
2025-10-10 10:45:11 +0800

    与Java的区别：支持多类继承

---
+++ day03/mutiInheritance.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Shape
+{
+    public:
+      void setWidth(int w){
+        width = w;
+      }
+      void setHeight(int h){
+        height = h;
+      }
+
+    protected:
+      int width;
+      int height;
+};
+
+class PaintCost
+{
+  public : 
+    int getCost(int area){
+      return area * 70;
+    }
+};
+
+class Rectangle: public Shape, public PaintCost{
+  public:
+    int getArea(){
+      return width * height;
+    }
+};
+
+int main(void){
+  Rectangle Rect;
+
+  Rect.setWidth(5);
+  Rect.setHeight(7);
+
+  int area = Rect.getArea();
+  cout << "Total area: " << area << endl;
+  cout << "Total paint cost: " << Rect.getCost(area) << endl;
+
+  return 0;
+
+
+}
