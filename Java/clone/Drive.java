7866a35f69d93a72295822a7443c3263d87bd1b9
LUICECHEN <1342993482@qq.com>
2025-10-14 18:44:25 +0800

    深拷贝和浅拷贝代码上的差异以及区别

--- Java/clone/Drive.java
+++ Java/clone/Drive.java
@@ -2,7 +2,7 @@ package clone;
 
 public class Drive {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Person person1 = new Person(new Address("武汉"));
+        Person person1 = new Person(new Address("广东"));
         Person person1Copy = person1.clone();
         // false
         System.out.println(person1.getAddress() == person1Copy.getAddress());
